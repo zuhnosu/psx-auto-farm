71b93a6954e30129a87e05e0ef2c28f4936af019
zuhnosu <97843307+zuhnosu@users.noreply.github.com>
2023-01-19 18:30:36 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,52 @@
+local Client = require(game.ReplicatedStorage.Library.Client)
+local Library = require(game.ReplicatedStorage.Library)
+local Orbs = getsenv(game.Players.LocalPlayer.PlayerScripts.Scripts.Game.Orbs)
+
+debug.setupvalue(Client.Network.Invoke, 1, function() return true end)
+debug.setupvalue(Client.Network.Fire, 1, function() return true end)
+
+function GetCoins(Area)
+    local Coins = {}
+    for i,v in next, Client.Network.Invoke('Get Coins') do 
+        if v.a == Area then 
+            Coins[i] = v
+        end 
+    end 
+    return Coins
+end 
+
+function GetPets()
+    return Client.PetCmds.GetEquipped() 
+end 
+
+for i,v in pairs(workspace.__THINGS.Orbs:GetChildren()) do 
+    Orbs.Collect(v)
+end 
+
+workspace.__THINGS.Orbs.ChildAdded:Connect(function(v)
+    if _G.AutoCollectOrbs then 
+        Orbs.Collect(v)
+    end 
+end)
+
+while true do 
+    if _G.AutoFarm then
+        local Pets = GetPets()
+        local Coins = GetCoins('Town')
+        for i,v in next, Coins do
+            if workspace.__THINGS.Coins:FindFirstChild(i) and _G.AutoFarm then 
+                for _,Pet in next, Pets do 
+                    spawn(function()
+                        if _G.AutoFarm then
+                            Client.Network.Invoke('Join Coin', i, {Pet.uid})
+                            Client.Network.Fire('Farm Coin', i, Pet.uid)
+                        end
+                    end)
+                end 
+            end 
+            repeat task.wait() until not workspace.__THINGS.Coins:FindFirstChild(i)
+        end 
+        task.wait()
+    end
+    task.wait()
+end 
